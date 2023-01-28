4b975f20ed3246342aabaabdf72c12f867d73d6c
Programming-1 <>
2023-01-28 15:30:11 -0600

    Made a simple command for SkiPlow Pneumatics

---
+++ src/main/java/frc/robot/subsystems/SkiPlow.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsBase;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
+
+public class SkiPlow extends SubsystemBase {
+  /** Creates a new SkiPlow. */
+  DoubleSolenoid skiPlowDoublePCM;
+  public SkiPlow() {
+    skiPlowDoublePCM = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+  }
+  public void pistonReverse() {
+    skiPlowDoublePCM.set(kReverse);
+  }
+  public void pistonForward() {
+    skiPlowDoublePCM.set(kForward);
+  }
+  public void pistonoff() {
+    skiPlowDoublePCM.set(kOff);
+  }
+  
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}

df1fa07986697afb4a502475363327d35fb85cac
Programming 02 <2491nomythic@gmail.com>
2023-02-11 19:19:12 -0600

    fix imports

--- src/main/java/frc/robot/subsystems/SkiPlow.java
+++ src/main/java/frc/robot/subsystems/SkiPlow.java
@@ -5,9 +5,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.PneumaticsBase;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class SkiPlow extends SubsystemBase {

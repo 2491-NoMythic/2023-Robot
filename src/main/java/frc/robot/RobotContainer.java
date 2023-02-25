f768bb0959d732487454568627ab71aa20bfabcc
Programming-1 <>
2023-02-25 10:06:47 -0600

    put the purple lights command into subsystems and into robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -45,6 +45,7 @@ import frc.robot.subsystems.RobotArmSubsystem;
 import edu.wpi.first.wpilibj.Preferences;
 import frc.robot.subsystems.SkiPlow;
 import frc.robot.subsystems.SubsystemLights;
+import frc.robot.Commands.PurpleLights;
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -129,7 +130,9 @@ public class RobotContainer {
   } 
   
   private void LightsInst() {
-    lightsSubsystem = new SubsystemLights(60);
+    lightsSubsystem = new SubsystemLights(52);
+    PurpleLights defaultLights = new PurpleLights(lightsSubsystem);
+    lightsSubsystem.setDefaultCommand(defaultLights);
   }
   private void DrivetrainInst(){
     drivetrain = new DrivetrainSubsystem();
@@ -150,6 +153,7 @@ public class RobotContainer {
     SmartDashboard.putNumber("Robot origin y", DriveConstants.DRIVE_ODOMETRY_ORIGIN.getY());
     SmartDashboard.putNumber("Robot origin rot", DriveConstants.DRIVE_ODOMETRY_ORIGIN.getRotation().getDegrees());
   }
+
   private void ArmInst(){
     arm = new RobotArmSubsystem();
     ControlArm = new RobotArmControl(arm);
@@ -236,9 +240,9 @@ public class RobotContainer {
           () -> getJoystickMagnitude(Z_AXIS, Z_ROTATE)));
     }
       if (LightsExists){
-        new Trigger(driveController::getTriangleButton).onTrue(Commands.runOnce(()->  {lightsSubsystem.lightsOut(); lightsSubsystem.setLights(29, 59, 200, 30, 30);}, lightsSubsystem));
-        new Trigger(driveController::getSquareButton).onTrue(Commands.runOnce(()->  {lightsSubsystem.lightsOut(); lightsSubsystem.setLights(0, 39, 0, 0, 100);}, lightsSubsystem));
-        new Trigger(driveController::getPSButton).onTrue(Commands.runOnce(()->  {lightsSubsystem.lightsOut(); lightsSubsystem.setLights(0, 59, 0, 0, 0);}, lightsSubsystem));
+        new Trigger(driveController::getTriangleButton).onTrue(Commands.runOnce(()->  {lightsSubsystem.lightsOut(); lightsSubsystem.setLights(29, 51, 200, 30, 30);}, lightsSubsystem));
+        new Trigger(driveController::getSquareButton).onTrue(Commands.runOnce(()->  {lightsSubsystem.lightsOut(); lightsSubsystem.setLights(0, 51, 0, 0, 100);}, lightsSubsystem));
+        new Trigger(driveController::getPSButton).onTrue(Commands.runOnce(()->  {lightsSubsystem.lightsOut(); lightsSubsystem.setLights(0, 51, 0, 0, 0);}, lightsSubsystem));
       }
   }
   /**

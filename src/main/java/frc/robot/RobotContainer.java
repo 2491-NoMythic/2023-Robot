c745d0cde178722bf2670e307d172a01da3b930c
2491NoMythic <2491nomythic@gmail.com>
2023-03-16 20:19:28 -0500

    now the end effector and intake code is made and on shuffleboard, and they have button consistency

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-
 import static frc.robot.settings.Constants.PS4Driver.DEADBAND_LARGE;
 import static frc.robot.settings.Constants.PS4Driver.DEADBAND_NORMAL;
 import static frc.robot.settings.Constants.PS4Driver.NO_INPUT;
@@ -58,14 +57,16 @@ import frc.robot.subsystems.SubsystemLights;
 import frc.robot.subsystems.SkiPlow;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  
 
   private Limelight limelight;
   private LimelightmotorSubsystem llmotor;
@@ -77,16 +78,16 @@ public class RobotContainer {
   private final PS4Controller opController;
 
   private Autos autos;
-  
+
   private RobotArmSubsystem arm;
   private RobotArmControl ControlArm;
-  
+
   private EndEffectorCommand endEffectorCommand;
-  private SkiPlowPneumatic skiplowcommand; 
+  private SkiPlowPneumatic skiplowcommand;
   private SkiPlow skiPlow;
 
   private EndEffector effector;
-  
+
   private SubsystemLights lightsSubsystem;
 
   public static HashMap<String, Command> eventMap;
@@ -97,11 +98,11 @@ public class RobotContainer {
   public static boolean LimelightMotorExists = Preferences.getBoolean("LimelightMotor", false);
   public static boolean DrivetrainExists = Preferences.getBoolean("Drivetrain", false);
   public static boolean LightsExists = Preferences.getBoolean("Lights", false);
-  
-  
-  
+
   public RobotContainer() {
-    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
     Preferences.initBoolean("Arm", false);
     Preferences.initBoolean("Drivetrain", false);
     Preferences.initBoolean("EndEffector", false);
@@ -117,38 +118,39 @@ public class RobotContainer {
     SmartDashboard.putNumber("endeffectorSpeed", 0.5);
     SmartDashboard.putNumber("skiplowRollerSpeed", 0.5);
 
-    if (ArmExists){
+    if (ArmExists) {
       ArmInst();
     }
-    if (EndEffectorExists){
+    if (EndEffectorExists) {
       EndEffectorInst();
     }
-    if (SkiPlowExists){
+    if (SkiPlowExists) {
       SkiPlowInst();
     }
-    if (LimelightExists){
+    if (LimelightExists) {
       LimelightInst();
     }
-    if (LimelightMotorExists){
+    if (LimelightMotorExists) {
       LimelightMotorInst();
     }
-    if (DrivetrainExists){
+    if (DrivetrainExists) {
       DrivetrainInst();
     }
-    if (LightsExists){
+    if (LightsExists) {
       LightsInst();
     }
     autoInit();
     configureBindings();
     configDashboard();
-  } 
-  
+  }
+
   private void LightsInst() {
     lightsSubsystem = new SubsystemLights(52);
     PurpleLights defaultLights = new PurpleLights(lightsSubsystem);
     lightsSubsystem.setDefaultCommand(defaultLights);
   }
-  private void DrivetrainInst(){
+
+  private void DrivetrainInst() {
     drivetrain = new DrivetrainSubsystem();
     // Set up the default command for the drivetrain.
     // The controls are for field-oriented driving:
@@ -157,11 +159,11 @@ public class RobotContainer {
     // Right stick Z axis -> rotation, clockwise, counterclockwise
     // Need to invert the joystick axis
     defaultDriveCommand = new Drive(
-      drivetrain,
-      () -> driveController.getL1Button(),
-      () -> modifyAxis(-driveController.getRawAxis(Y_AXIS), DEADBAND_NORMAL),
-      () -> modifyAxis(-driveController.getRawAxis(X_AXIS), DEADBAND_NORMAL),
-      () -> modifyAxis(-driveController.getRawAxis(Z_AXIS), DEADBAND_NORMAL));
+        drivetrain,
+        () -> driveController.getL1Button(),
+        () -> modifyAxis(-driveController.getRawAxis(Y_AXIS), DEADBAND_NORMAL),
+        () -> modifyAxis(-driveController.getRawAxis(X_AXIS), DEADBAND_NORMAL),
+        () -> modifyAxis(-driveController.getRawAxis(Z_AXIS), DEADBAND_NORMAL));
     drivetrain.setDefaultCommand(defaultDriveCommand);
     SmartDashboard.putData(drivetrain);
     SmartDashboard.putNumber("Robot origin x", DriveConstants.DRIVE_ODOMETRY_ORIGIN.getX());
@@ -169,7 +171,8 @@ public class RobotContainer {
     SmartDashboard.putNumber("Robot origin rot", DriveConstants.DRIVE_ODOMETRY_ORIGIN.getRotation().getDegrees());
     SmartDashboard.putNumber("Precision Multiplier", 0.5);
   }
-  private void ArmInst(){
+
+  private void ArmInst() {
     arm = new RobotArmSubsystem();
     ControlArm = new RobotArmControl(arm, 
     () -> opController.getRightY(), 
@@ -177,14 +180,14 @@ public class RobotContainer {
     arm.setDefaultCommand(ControlArm);
   }
   private void EndEffectorInst(){
-    effector = new EndEffector();
+    effector = new EndEffector(SmartDashboard.getNumber("endeffectorSpeed", 0.5));
     endEffectorCommand = new EndEffectorCommand(effector, 
     () -> opController.getRightY(), 
     SmartDashboard.getNumber("endeffectorSpeed", 0.5));
 
   }
   private void SkiPlowInst(){
-    skiPlow = new SkiPlow();
+    skiPlow = new SkiPlow(SmartDashboard.getNumber("skiplowRollerSpeed", 0.5));
     skiplowcommand = new SkiPlowPneumatic(skiPlow, 
     () -> opController.getL2ButtonPressed(), 
     () -> opController.getCrossButtonPressed(), 
@@ -194,15 +197,17 @@ public class RobotContainer {
     );
     skiPlow.setDefaultCommand(skiplowcommand);  
   }
-  private void LimelightInst(){
+
+  private void LimelightInst() {
     limelight = Limelight.getInstance();
   }
-  private void LimelightMotorInst(){
+
+  private void LimelightMotorInst() {
     llmotor = new LimelightmotorSubsystem();
     Command defaultllmotorCommand = new RunViaLimelightCommand(llmotor);
     llmotor.setDefaultCommand(defaultllmotorCommand);
   }
-  
+
   private void autoInit() {
     autos = Autos.getInstance();
     if (DrivetrainExists) {
@@ -212,7 +217,13 @@ public class RobotContainer {
       if (SkiPlowExists) {
         eventMap.put("IntakeDown", new SequentialCommandGroup(new InstantCommand(skiPlow::pistonDown, skiPlow), new WaitCommand(0.75)));
         eventMap.put("IntakeUp", new SequentialCommandGroup(new InstantCommand(skiPlow::pistonUp, skiPlow), new WaitCommand(0.5)));
+        eventMap.put("IntakeRollerIn", new SequentialCommandGroup(new InstantCommand(skiPlow::rollerCube, skiPlow)));
+        eventMap.put("IntakeOut", new SequentialCommandGroup(new InstantCommand(skiPlow::rollerCone, skiPlow)));
         // eventMap.put("skiPlowLock", TODO add command);
+      }
+      if (EndEffectorExists) {
+        eventMap.put("EndEffectorIn", new SequentialCommandGroup(new InstantCommand(effector::rollerIn, effector)));
+        eventMap.put("EndEffectorOut", new SequentialCommandGroup(new InstantCommand(effector::rollerOut, effector)));
       }
       if (LightsExists) {
         // eventMap.put("TODO add command", TODO add command);
@@ -226,9 +237,15 @@ public class RobotContainer {
       SmartDashboard.putData(autoChooser);
     }
   }
-  
-  private void configDashboard() {}
-  /**Takes both axis of a joystick, returns an angle from -180 to 180 degrees, or {@link Constants.PS4Driver.NO_INPUT} (double = 404.0) if the joystick is at rest position*/
+
+  private void configDashboard() {
+  }
+
+  /**
+   * Takes both axis of a joystick, returns an angle from -180 to 180 degrees, or
+   * {@link Constants.PS4Driver.NO_INPUT} (double = 404.0) if the joystick is at
+   * rest position
+   */
   private double getJoystickDegrees(int horizontalAxis, int verticalAxis) {
     double xAxis = MathUtil.applyDeadband(-driveController.getRawAxis(horizontalAxis), DEADBAND_LARGE);
     double yAxis = MathUtil.applyDeadband(-driveController.getRawAxis(verticalAxis), DEADBAND_LARGE);
@@ -237,20 +254,27 @@ public class RobotContainer {
     }
     return NO_INPUT;
   }
-  /**Takes both axis of a joystick, returns a double from 0-1 */
+
+  /** Takes both axis of a joystick, returns a double from 0-1 */
   private double getJoystickMagnitude(int horizontalAxis, int verticalAxis) {
     double xAxis = MathUtil.applyDeadband(-driveController.getRawAxis(horizontalAxis), DEADBAND_NORMAL);
     double yAxis = MathUtil.applyDeadband(-driveController.getRawAxis(verticalAxis), DEADBAND_NORMAL);
-    return Math.min(1.0, (Math.sqrt(Math.pow(xAxis, 2) + Math.pow(yAxis, 2)))); // make sure the number is not greater than 1
+    return Math.min(1.0, (Math.sqrt(Math.pow(xAxis, 2) + Math.pow(yAxis, 2)))); // make sure the number is not greater
+                                                                                // than 1
   }
-  
+
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4DriverController
-   * PS4Driver} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4DriverController
+   * PS4Driver} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
@@ -258,7 +282,7 @@ public class RobotContainer {
     if (DrivetrainExists) {
       new Trigger(driveController::getPSButton).onTrue(Commands.runOnce(drivetrain::zeroGyroscope, drivetrain));
       // new Trigger(driveController::getTriangleButton).onTrue(Commands.runOnce(() ->
-      //     autos.moveToPose(DriveConstants.DRIVE_ODOMETRY_ORIGIN)));
+      // autos.moveToPose(DriveConstants.DRIVE_ODOMETRY_ORIGIN)));
       new Trigger(driveController::getCrossButton).onTrue(Commands.runOnce(drivetrain::pointWheelsInward, drivetrain));
       new Trigger(driveController::getR1Button).whileTrue(new DriveRotateToAngleCommand(drivetrain,
           () -> modifyAxis(-driveController.getRawAxis(Y_AXIS), DEADBAND_NORMAL),
@@ -266,32 +290,54 @@ public class RobotContainer {
           () -> getJoystickDegrees(Z_AXIS, Z_ROTATE),
           () -> getJoystickMagnitude(Z_AXIS, Z_ROTATE)));
       new Trigger(driveController::getR2Button).whileTrue(new DriveOffsetCenterCommand(
-        drivetrain,
-        () -> driveController.getL1Button(),
-        () -> modifyAxis(-driveController.getRawAxis(Y_AXIS), DEADBAND_NORMAL),
-        () -> modifyAxis(-driveController.getRawAxis(X_AXIS), DEADBAND_NORMAL),
-        () -> modifyAxis(-driveController.getRawAxis(Z_AXIS), DEADBAND_NORMAL), 
-        new Translation2d(1,0)));
+          drivetrain,
+          () -> driveController.getL1Button(),
+          () -> modifyAxis(-driveController.getRawAxis(Y_AXIS), DEADBAND_NORMAL),
+          () -> modifyAxis(-driveController.getRawAxis(X_AXIS), DEADBAND_NORMAL),
+          () -> modifyAxis(-driveController.getRawAxis(Z_AXIS), DEADBAND_NORMAL),
+          new Translation2d(1, 0)));
       new Trigger(driveController::getSquareButton).whileTrue(new DriveBalanceCommand(
-        drivetrain))
-        ;
-      
+          drivetrain));
+
     }
-    if (LightsExists){
-        new Trigger(opController::getTriangleButton).whileTrue(Commands.run(()->  {lightsSubsystem.lightsOut(); lightsSubsystem.setLights(0, 51, 100, 64, 0);}, lightsSubsystem));
-        new Trigger(opController::getSquareButton).whileTrue(Commands.run(()->  {lightsSubsystem.lightsOut(); lightsSubsystem.setLights(0, 51, 0, 0, 100);}, lightsSubsystem));
+    if (LightsExists) {
+      new Trigger(opController::getTriangleButton).whileTrue(Commands.run(() -> {
+        lightsSubsystem.lightsOut();
+        lightsSubsystem.setLights(0, 51, 100, 64, 0);
+      }, lightsSubsystem));
+      new Trigger(opController::getSquareButton).whileTrue(Commands.run(() -> {
+        lightsSubsystem.lightsOut();
+        lightsSubsystem.setLights(0, 51, 0, 0, 100);
+      }, lightsSubsystem));
     }
-    if (ArmExists) {    
-      new Trigger(opController::getL1Button).onTrue(Commands.runOnce(()-> {arm.setShoulderPower(-0.1);}, arm)).onFalse(Commands.runOnce(()-> {arm.setShoulderPower(0);}, arm));
-      new Trigger(opController::getL2Button).onTrue(Commands.runOnce(()-> {arm.setShoulderPower(0.1);}, arm)).onFalse(Commands.runOnce(()-> {arm.setShoulderPower(0);}, arm));
-      new Trigger(opController::getShareButton).onTrue(Commands.runOnce(()-> {arm.setElbowPower(-0.1);}, arm)).onFalse(Commands.runOnce(()-> {arm.setElbowPower(0);}, arm));
-      new Trigger(opController::getOptionsButton).onTrue(Commands.runOnce(()-> {arm.setElbowPower(0.1);}, arm)).onFalse(Commands.runOnce(()-> {arm.setElbowPower(0);}, arm));
+    if (ArmExists) {
+      new Trigger(opController::getL1Button).onTrue(Commands.runOnce(() -> {
+        arm.setShoulderPower(-0.1);
+      }, arm)).onFalse(Commands.runOnce(() -> {
+        arm.setShoulderPower(0);
+      }, arm));
+      new Trigger(opController::getL2Button).onTrue(Commands.runOnce(() -> {
+        arm.setShoulderPower(0.1);
+      }, arm)).onFalse(Commands.runOnce(() -> {
+        arm.setShoulderPower(0);
+      }, arm));
+      new Trigger(opController::getShareButton).onTrue(Commands.runOnce(() -> {
+        arm.setElbowPower(-0.1);
+      }, arm)).onFalse(Commands.runOnce(() -> {
+        arm.setElbowPower(0);
+      }, arm));
+      new Trigger(opController::getOptionsButton).onTrue(Commands.runOnce(() -> {
+        arm.setElbowPower(0.1);
+      }, arm)).onFalse(Commands.runOnce(() -> {
+        arm.setElbowPower(0);
+      }, arm));
     }
     if (SkiPlowExists) {
       BooleanSupplier tmp = opController::getR1Button;
       BooleanSupplier tmp2 = () -> opController.getR1Button();
     }
   }
+
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
@@ -299,8 +345,9 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     return autoChooser.getSelected();
-   }
-   private double modifyAxis(double value, double deadband) {
+  }
+
+  private double modifyAxis(double value, double deadband) {
     // Deadband
     value = MathUtil.applyDeadband(value, deadband);
     // Square the axis
@@ -310,16 +357,19 @@ public class RobotContainer {
     }
     return value;
   }
+
   public void robotInit() {
-    if(DrivetrainExists){
+    if (DrivetrainExists) {
       drivetrain.zeroGyroscope();
     }
   }
+
   public void teleopInit() {
-    if(DrivetrainExists){
+    if (DrivetrainExists) {
       drivetrain.pointWheelsForward();
     }
   }
+
   public void teleopPeriodic() {
     SmartDashboard.putNumber("Match Timer", Timer.getMatchTime());
   }

64d822cd7be4fa2d8bde600c9195c0399879514b
Emilio Alvarez <e.l.alvarez@icloud.com>
2023-03-30 00:28:27 -0500

    implement last two pose commands

--- src/main/java/frc/robot/Commands/arm/DropLow.java
+++ src/main/java/frc/robot/Commands/arm/DropLow.java
@@ -4,16 +4,30 @@
 
 package frc.robot.Commands.arm;
 
+import static edu.wpi.first.wpilibj2.command.Commands.runOnce;
+import static edu.wpi.first.wpilibj2.command.Commands.waitUntil;
+import static frc.robot.settings.Constants.Poses.DROP_LOW;
+import static frc.robot.settings.Constants.Poses.RESET;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.SkiPlow;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class DropLow extends SequentialCommandGroup {
+  private static final double TIMEOUT = 2.0;
+
   /** Creates a new DropLow. */
-  public DropLow() {
+  public DropLow(ArmSubsystem arm, SkiPlow intake) {
     // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    addCommands(
+        runOnce(intake::pistonDown, intake).unless(() -> !DROP_LOW.isRequiresIntakeDown()),
+        runOnce(() -> arm.setDesiredSholderPose(RESET), arm).unless(() -> !arm.isExtended()),
+        runOnce(() -> arm.setDesiredElbowPose(DROP_LOW), arm),
+        waitUntil(arm::isElbowAtTarget).withTimeout(TIMEOUT),
+        runOnce(() -> arm.setDesiredSholderPose(DROP_LOW), arm),
+        runOnce(intake::pistonUp, intake));
   }
 }
--- src/main/java/frc/robot/Commands/arm/ShelfCone.java
+++ src/main/java/frc/robot/Commands/arm/ShelfCone.java
@@ -4,16 +4,42 @@
 
 package frc.robot.Commands.arm;
 
+import static edu.wpi.first.wpilibj2.command.Commands.runOnce;
+import static edu.wpi.first.wpilibj2.command.Commands.none;
+import static edu.wpi.first.wpilibj2.command.Commands.either;
+import static edu.wpi.first.wpilibj2.command.Commands.waitUntil;
+import static frc.robot.settings.Constants.Poses.SHELF_CONE;
+import static frc.robot.settings.Constants.Poses.RESET;
+import static frc.robot.settings.Constants.Poses.AVOID_POST;
+
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.SkiPlow;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class ShelfCone extends SequentialCommandGroup {
-  /** Creates a new IntakeConeShelf. */
-  public ShelfCone() {
+  private static final double TIMEOUT = 2.0;
+
+  /** Creates a new ShelfCone. */
+  public ShelfCone(ArmSubsystem arm, SkiPlow intake) {
     // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    addCommands(
+        runOnce(intake::pistonDown, intake).unless(() -> !SHELF_CONE.isRequiresIntakeDown()),
+        either(
+            Commands.sequence(
+                runOnce(() -> arm.setDesiredSholderPose(RESET), arm),
+                waitUntil(arm::isShoulderAtTarget).withTimeout(TIMEOUT),
+                runOnce(() -> arm.setDesiredElbowPose(AVOID_POST), arm)),
+            none(),
+            arm::isExtended), // TODO: could be a reset command ?
+        runOnce(() -> arm.setDesiredSholderPose(AVOID_POST), arm),
+        waitUntil(arm::isShoulderAtTarget).withTimeout(TIMEOUT),
+        runOnce(() -> arm.setDesiredElbowPose(SHELF_CONE), arm),
+        waitUntil(arm::isElbowAtTarget).withTimeout(TIMEOUT),
+        runOnce(() -> arm.setDesiredSholderPose(SHELF_CONE), arm),
+        runOnce(intake::pistonUp, intake));
   }
 }

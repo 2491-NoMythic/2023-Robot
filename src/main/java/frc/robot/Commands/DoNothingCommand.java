377a37ab65bd8edd1fe5b5931f6d3881563352dd
Chris Ward <chris.ward@improving.com>
2023-04-06 15:21:05 -0500

    Adding a new command that does nothing

---
+++ src/main/java/frc/robot/Commands/DoNothingCommand.java
@@ -0,0 +1,35 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class DoNothingCommand extends CommandBase {
+    
+  private double endTimeInMillis;
+  private final double secondsToWait;
+
+  /**
+   * Creates a new DoNothingCommand. This command will do nothing, and end after the specified duration.
+   * Should run like any command and not interrupt other running commands.
+   *
+   * @param seconds the time to wait, in seconds
+   */
+  public DoNothingCommand(double seconds) {
+    secondsToWait = seconds;
+  }
+
+  @Override
+  public void initialize() {
+    endTimeInMillis = System.currentTimeMillis() + secondsToWait * 1000;
+  }
+
+  @Override
+  public boolean isFinished() {
+    return System.currentTimeMillis() >= endTimeInMillis;
+  }
+
+  @Override
+  public boolean runsWhenDisabled() {
+    return true;
+  }
+
+}
